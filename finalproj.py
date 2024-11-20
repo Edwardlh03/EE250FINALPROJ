6a70400c024dc8204e5e823b88c1231fc1809365
Jack Mitchell <jackmitc@usc.edu>
2024-11-19 17:24:59 -0800

    commit seven

--- finalproj.py
+++ finalproj.py
@@ -3,12 +3,19 @@ import time
 import math
 dht_sensor_port = 7
 ultrasonic_ranger = 4
+potentiometer = 2
 light_sensor = 1
 sound_sensor = 0
 pinMode(light_sensor,"INPUT")
+
+grove_vcc = 5
+adc_ref = 5
+
+full_angle = 300
+
 while True:
     try:
-        [temp, hum] = dht(4,0)
+        [temp, hum] = dht(7,0)
         if math.isnan(temp) == False and math.isnan(hum) == False:
             print("temp =", temp, "C\thumidity =", hum,"%")
         # [temp, hum] = dht(dht_sensor_port,0)
@@ -23,5 +30,11 @@ while True:
         # sound_value = analogRead(sound_sensor)
         # print(light_value, 'light units,', sound_value, 'sound units')
         # time.sleep(1)
+        sensor_value = analogRead(potentiometer)
+        voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
+        degrees = round((voltage * full_angle) / grove_vcc, 2)
+        print(voltage, degrees)
+        time.sleep(1)
+
     except Error:
         pass
