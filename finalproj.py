d3e00a31165cc59326a0f242e5fd6799d4470861
Jack Mitchell <jackmitc@usc.edu>
2024-11-19 17:38:32 -0800

    aosfj

--- finalproj.py
+++ finalproj.py
@@ -32,14 +32,17 @@ while True:
         # sound_value = analogRead(sound_sensor)
         # print(light_value, 'light units,', sound_value, 'sound units')
         # time.sleep(1)
-        print(potentiometer)
-        print(analogRead(potentiometer))
-        sensor_value = analogRead(potentiometer)
-        voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
-        degrees = round((voltage * full_angle) / grove_vcc, 2)
-        print(voltage, degrees)
-        print(round((float)(analogRead(potentiometer)) * adc_ref / 1023, 2), round((round((float)(analogRead(potentiometer)) * adc_ref / 1023, 2) * full_angle) / grove_vcc, 2))
-        time.sleep(1)
+        read = analogRead(potentiometer)
+        if(read != 65536):
+            print(read)
+        # print(potentiometer)
+        # print(analogRead(potentiometer))
+        # sensor_value = analogRead(potentiometer)
+        # voltage = round((float)(sensor_value) * adc_ref / 1023, 2)
+        # degrees = round((voltage * full_angle) / grove_vcc, 2)
+        # print(voltage, degrees)
+        # print(round((float)(analogRead(potentiometer)) * adc_ref / 1023, 2), round((round((float)(analogRead(potentiometer)) * adc_ref / 1023, 2) * full_angle) / grove_vcc, 2))
+        # time.sleep(1)
 
     except Error:
         pass
